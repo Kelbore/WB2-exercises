6774c476c41f1e1819fe1077f2f4383ab0eaf14c
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-04-25 13:50:26 -0400

    the second commit

--- MathScripts/area_of_circle.js
+++ MathScripts/area_of_circle.js
@@ -2,7 +2,8 @@
 var radius = 20;
 var pi = 3.14;
 
-var areaOfCircle = pi * radius * radius;
+/* perform operation */
+var areaOfCircle = pi * radius**2;
 
 /* output the area of a circle */
 console.log("The area of a circle with radius" + " ", radius + " " + "is" + " " + areaOfCircle);
--- MathScripts/area_of_rectangle.js
+++ MathScripts/area_of_rectangle.js
@@ -1,8 +1,10 @@
 /* calculate area of rectangle */
 var length = 15;
-var width = 10;
+var height = 10;
 
-var areaOfRectangle = length * width;
+var areaOfRectangle = length * height;
 
 /* output the area of rectangle */
+console.log("length" + " " + length);
+console.log("height" + " " + height);
 console.log("The area of the rectangle is" + " " + areaOfRectangle);
