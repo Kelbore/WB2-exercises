b36ac6b63fb966058e476ad325aa2c6ad6b7156a
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-04-26 09:54:21 -0400

    created pay_rules.js file

---
+++ IfScripts/pay_rules.js
@@ -0,0 +1,12 @@
+var payRate = 17.30;
+var hoursWorked = 45;
+var pay;
+
+if(hoursWorked > 40) {
+    pay = (payRate * 40) + ((payRate * 1.5)*(hoursWorked - 40));
+    console.log('Your payment for this week is' + ' ' + pay.toFixed(2));
+}
+else {
+    pay = payRate * hoursWorked;
+    console.log('Your payment for this week is' + ' ' + pay.toFixed(2));
+}
