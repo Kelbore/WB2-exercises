75cfcf78b6bf2df8d7431a8aa0a10eb5bc5b306c
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-04-26 15:32:25 -0400

    modified show_major.js file

--- IfScripts/show_major.js
+++ IfScripts/show_major.js
@@ -1,33 +1,33 @@
-var studentName1 = 'John';
-var studentName2 = 'Alex';
-var studentName3 = 'David';
-var studentName4 = 'James';
-var studentName5 = 'Solomon';
-var studentMajor = 'CSI';
+var studentName = 'John';
+var studentMajor = 'HIST';
 
 if (studentMajor == 'BIOL') {
-    console.log('Student:' + studentName1);
+    console.log('Student:' + studentName);
     console.log('Major:' + 'Biology');
     console.log('Advising Location:' + 'Science Bldg, Room 310');
 }
 else if (studentMajor == 'CSI') {
-    console.log('Student:' + studentName2);
+    console.log('Student:' + studentName);
     console.log('Major:' + 'Computer Science');
     console.log('Advising Location:' + 'Sheppard Hall, Room 314');
 }
 else if (studentMajor == 'ENG') {
-    console.log('Student:' + studentName3);
+    console.log('Student:' + studentName);
     console.log('Major:' + 'English');
     console.log('Advising Location:' + 'Kerr Hall, Room 201');
 }
 else if (studentMajor == 'HIST') {
-    console.log('Student:' + studentName4);
+    console.log('Student:' + studentName);
     console.log('Major:' + 'History');
     console.log('Advising Location:' + 'Kerr Hall, Room 114');
 }
 else if (studentMajor == 'MKT') {
-    console.log('Student:' + studentName5);
+    console.log('Student:' + studentName);
     console.log('Major:' + 'Marketing');
     console.log('Advising Location:' + 'Westly Hall, Room 310');
 }
-
+else {
+    console.log('Student:' + studentName);
+    console.log('Major:' + 'Unknown');
+    console.log('Advising Location:' + 'Unknown');
+}
