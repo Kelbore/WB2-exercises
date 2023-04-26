6802ae5da209a003b0d10f1a25726c1a9e3370b0
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-04-26 13:16:59 -0400

    modified greetin.js file

--- IfScripts/greeting.js
+++ IfScripts/greeting.js
@@ -1,14 +1,14 @@
-var currentHours = 19;
+var currentHours = 20;
 var ampm = 'PM';
 
 
-if(0 <= currentHours <= 10 && ampm == 'AM') {
+if(currentHours <= 10 && ampm == 'AM') {
     console.log('Good morning!');
 }
-else if(10 <= currentHours <= 12 && ampm == 'AM') {
+else if(currentHours <= 12 && ampm == 'AM') {
     console.log('Good day!');
 }
-else if(12 < currentHours < 17 && ampm == 'PM') {
+else if(currentHours < 17 && ampm == 'PM') {
     console.log('Good day!');
 }
 else{
